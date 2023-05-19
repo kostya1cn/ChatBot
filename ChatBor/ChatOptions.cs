b67e38d75396e9049aeffaec4d3d9e7587ca81a2
Константин Тарасов <kostya1cn@gmail.com>
2023-05-19 10:03:57 +0200

    Добавьте файлы проекта.

---
+++ ChatBor/ChatOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatBot
+{
+    public class ChatOptions
+    {
+        public string ApiKey { get; set; }
+        public string ApiUrl { get; set; }
+        public string GptModel { get; set; }
+    }
+}
---
+++ ChatBor/Pages/Index.razor.cs
@@ -0,0 +1,55 @@
+﻿using ChatBot.Models;
+using ChatBot.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace ChatBot.Pages
+{
+    public partial class Index : ComponentBase
+    {
+
+        [Inject]
+        public OpenAIService? OpenAIService { get; set; }
+
+        private string _userQuestion = string.Empty;
+        private readonly List<Message> _conversationHistory = new();
+        private bool _isSendingMessage;
+
+        private async Task HandleKeyPress(KeyboardEventArgs e)
+        {
+            if (e.Key is not "Enter") return;
+            await SendMessage();
+        }
+
+        private async Task SendMessage()
+        {
+            if (string.IsNullOrWhiteSpace(_userQuestion)) return;
+            AddUserQuestionToConversation();
+            StateHasChanged();
+            await CreateCompletion();
+            ClearInput();
+            StateHasChanged();
+        }
+
+        private void ClearInput() => _userQuestion = string.Empty;
+
+        private void ClearConversation()
+        {
+            ClearInput();
+            _conversationHistory.Clear();
+        }
+
+        private async Task CreateCompletion()
+        {
+            _isSendingMessage = true;
+            var assistantResponse = await OpenAIService.CreateChatCompletion(_conversationHistory);
+            _conversationHistory.Add(assistantResponse);
+            _isSendingMessage = false;
+        }
+
+        private void AddUserQuestionToConversation()
+            => _conversationHistory.Add(new Message { role = "user", content = _userQuestion });
+
+        public List<Message> Messages => _conversationHistory.Where(c => c.role is not "system").ToList();
+    }
+}
